cb9566fee976ff3665eebd8d0cb65d00278656f3
kidp2h <thjnhsoajca@gmail.com>
2024-05-24 14:48:55 +0700

    refactor(prisma): add middleware hash password employee

--- src/common/prisma/prisma.service.ts
+++ src/common/prisma/prisma.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class PrismaService
@@ -16,8 +17,20 @@ export class PrismaService
       ],
     });
   }
-  onModuleInit() {
-    this.$connect();
+  async onModuleInit() {
+    await this.$connect();
+    this.$use(async (params, next) => {
+      // console.log(params);
+
+      if (params.action == 'create' && params.model == 'Employee') {
+        const employee = params.args.data;
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(employee.password, salt);
+        employee.password = hash;
+        params.args.data = employee;
+      }
+      return next(params);
+    });
   }
   onModuleDestroy() {
     this.$disconnect();
